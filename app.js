4a568b36604714867bf77d4b45298b2fc6c6b3d9
Andrew Pickelsimer <apickelsimer@apigee.com>
2017-02-28 21:29:31 -0500

    mutual tls

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+// developed by apickelsimer
+// 2-28-17
+// API usage
+// $curl --insecure --cert-type pem --cert user.p12 "https://localhost:9000/" --pass "Password1"
+
 var express = require('express');
+var fs = require('fs');
+var https = require('https');
+var clientCertificateAuth = require('client-certificate-auth');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var opts = {
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.pem'),
+  ca: fs.readFileSync('./ssl/ca.pem'),
+  requestCert: true,
+  rejectUnauthorized: false,
+  passphrase: "Password1"
+};
 
 var app = express();
 
@@ -11,67 +28,59 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/service/:service/', function(req, res, next) {
-  var service = req.param('service');
-  var path = './data/' + req.param('service') + '.json';
-  var a = require(path);
-  switch(service){
-     case 'weather' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 10);
-             break;
-     case 'flightdetails' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 49);
-             break;
-     case 'profile' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 118);
-             break;
-     case 'flights' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 256);
-             break;
-    case 'flightdepartdetails' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 399);
-             break;
-    case 'airports' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 681);
-             break;
-    case 'flighthistory' :
-     		 setTimeout(function () {
-    			res.json(a);
-  			 }, 1481);
-             break;
-     default:
-            res.json(a);
-            break;
+  //mutual tls
+  if(req.client.authorized) {
+      //mock end points
+      var service = req.param('service');
+      var path = './data/' + req.param('service') + '.json';
+      var a = require(path);
+      switch(service){
+         case 'weather' :
+             setTimeout(function () {
+              res.json(a);
+             }, 10);
+                 break;
+         case 'flightdetails' :
+             setTimeout(function () {
+              res.json(a);
+             }, 49);
+                 break;
+         case 'profile' :
+             setTimeout(function () {
+              res.json(a);
+             }, 118);
+                 break;
+         case 'flights' :
+             setTimeout(function () {
+              res.json(a);
+             }, 256);
+                 break;
+        case 'flightdepartdetails' :
+             setTimeout(function () {
+              res.json(a);
+             }, 399);
+                 break;
+        case 'airports' :
+             setTimeout(function () {
+              res.json(a);
+             }, 681);
+                 break;
+        case 'flighthistory' :
+             setTimeout(function () {
+              res.json(a);
+             }, 1481);
+                 break;
+         default:
+                res.json(a);
+                break;
+      }
+    }
+    else {
+    console.log("Response sent. Unauthorized 401");
+    res.send("unauthorized or invalid cert");
   }
 
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err);
-});
-
-app.listen(3000, function () {
-  console.log('Flight Service api listening on port 3000!')
-});
-
-module.exports = app;
+const port = process.env.PORT || 9000;
+https.createServer(opts, app).listen(port);
